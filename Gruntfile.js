c6f806c6ca03c76bad9240c7e2e4c92c99721c08
Peter Nochisaki <pnochisaki@gmail.com>
2015-03-03 14:35:14 -0800

    initial push

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    bower_concat: {
+      all: {
+        dest: 'scripts/build/_bower.js',
+        cssDest: 'build/_bower.css',
+        exclude: [
+            'jquery',
+            'modernizr'
+        ],
+        dependencies: {
+          'underscore': 'jquery',
+          'backbone': 'underscore',
+          'jquery-mousewheel': 'jquery'
+        },
+        mainFiles: {
+          'skrollr-stylesheets': 'src/skrollr.stylesheets.js'
+        },
+        bowerOptions: {
+          relative: false
+        }
+      }
+    },
+
+
+
+    wiredep: {
+      task: {
+        src: [
+          '*.html',   // .html support...
+        ]
+      }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      sass: {
+        files: ['sass/**'],
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'watch']);
+};
+
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+    var contactModal = $('#contactModal');
+    var contactButton = $('#contactButton');
+    var closeModal = $('#closeForm');
+    contactButton.on('click', function() {
+        contactModal.toggleClass('display');
+    });
+    closeModal.on('click', function() {
+        contactModal.removeClass('display');
+    });
+});
